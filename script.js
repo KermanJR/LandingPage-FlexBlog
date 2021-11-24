2f0c7190a1fa13b2a545a5adbcee18954a95d404
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2021-11-24 19:34:17 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import scrollIndicator from "./modules/scrollIndicator.js";
-import scrollSuave from "./modules/scrollSuave.js"
-import fecharModal from "./modules/modal.js"
-
-scrollIndicator();
-scrollSuave();
-fecharModal();
