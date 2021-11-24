b3342d0515083c0b664a6958d435c854e7bf1ffb
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2021-11-24 19:33:42 -0300

    Delete scrollIndicator.js

--- scrollIndicator.js
+++
@@ -1,9 +0,0 @@
-
-export default function scrollIndicator(){
-    const indicator = document.querySelector('.indicator');
-    const maxHeight = document.body.scrollHeight - window.innerHeight;
-    window.addEventListener('scroll', function(){
-        const p = (window.scrollY/maxHeight)*100;
-        indicator.style.width = `${p}%`;
-    })
-}
